fb5859406d1a302a01c95b6b2a55ca23f44168a4
unknown <lienapuey@gmail.com>
2023-02-17 22:45:29 +0100

    styles

--- src/app/components/starship-detail/starship-detail.component.ts
+++ src/app/components/starship-detail/starship-detail.component.ts
@@ -1,24 +1,6 @@
 import { ShipsService } from 'src/app/services/ships.service';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
-
-interface StarshipDetail {
-name: string,
-model: string,
-manufacturer: string;
-cost: string;
-length: number;
-maxSpeed: number;
-crew: string;
-passengers: number;
-capacity: number;
-consumables: string;
-rating: string;
-mglt: number;
-class: string;
-}
 
 @Component({
   selector: 'app-starship-detail',
@@ -27,19 +9,15 @@ class: string;
 })
 export class StarshipDetailComponent implements OnInit{
   starshipId: string = '';
-  starshipDetail!: StarshipDetail;
-  constructor(private route: ActivatedRoute, private http: HttpClient, private shipService: ShipsService){}
+  starshipDetail: any;
+  constructor(private shipService: ShipsService){}
 ngOnInit(): void {
-  // this.shipService.getDetail()
-  // .then((resp:any) => {
-  //   console.log
-  // })
-    this.starshipId = this.route.snapshot.queryParamMap.get('id') ?? '';
-  
-  console.log(this.starshipId, 'id');
-  this.http.get(this.starshipId).subscribe((resp:any)=>{
-    this.starshipDetail=resp;
-  })
+  this.shipService.getDetail()
+  .then((resp:any)=>{
+    console.log(resp, 'resultado');
+    this.starshipDetail = resp;
+  });
+
 
 }
 }
--- src/app/services/ships.service.ts
+++ src/app/services/ships.service.ts
@@ -2,21 +2,24 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ShipsService {
   starshipId: string = '';
-  constructor( private http: HttpClient, private route: ActivatedRoute ) { }
-
-  getShips(){
-    return this.http.get('https://swapi.dev/api/starships/').toPromise();
-  }
-// https://starpi.herokuapp.com/
-  getDetail(){
+  constructor( private http: HttpClient, private route: ActivatedRoute ) { 
     this.starshipId = this.route.snapshot.queryParamMap.get('id') ?? '';
-  
-    console.log(this.starshipId, 'id');
-    this.http.get(this.starshipId).toPromise();
   }
+
+  getShips(): Promise<Object | undefined>{
+    return this.http.get('https://starpi.herokuapp.com/starpi/starships/').toPromise();
+  }
+
+  getDetail(): Promise<Object | undefined>{
+
+    return this.http.get(this.starshipId).toPromise();
+  }
+
+
 }
